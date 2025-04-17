3c139ebd8fa6086dbe5aad363e4c60223e3b5623
NikitaKuzlyaev <87369806+NikitaKuzlyaev@users.noreply.github.com>
2025-04-17 20:09:59 +0700

    Update k.py

--- k.py
+++ k.py
@@ -1,1 +1,14 @@
+import sys
 
+m = int(sys.stdin.readline())
+c = list(map(int, sys.stdin.readline().split()))
+
+su = sum(c)
+mx = max(c)
+
+su = su - mx
+
+if mx > su + 2:
+    print('NO')
+else:
+    print('YES')
