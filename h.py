b4fa3251a6766cf4479433c2a9e52a361f684959
NikitaKuzlyaev <87369806+NikitaKuzlyaev@users.noreply.github.com>
2025-04-17 20:19:47 +0700

    Update h.py

--- h.py
+++ h.py
@@ -1,1 +1,10 @@
+t = int(input())
 
+for _ in range(t):
+    s = str(input())
+
+    res = ""
+    for idx, ch in enumerate(s, start=1):
+        res += "abcde"[(idx + ord(ch) - ord('a')) % 5]
+
+    print(res)
