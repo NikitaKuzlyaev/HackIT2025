340e392b00fc06d8f94d92e2fa23ab79a0b08a77
NikitaKuzlyaev <87369806+NikitaKuzlyaev@users.noreply.github.com>
2025-05-12 19:19:56 +0700

    Update l.py

--- l.py
+++ l.py
@@ -1,59 +1,40 @@
-import math
 from typing import List, Tuple
+import sys
+ 
+sys.setrecursionlimit(10 ** 5)
  
  
 def solve(n_dots: int, dots: list[tuple[int, int]]) -> float | int:
-    distances = []
-    size = [1] * n_dots
-    comp = [i for i in range(n_dots)]
+    def dis(p1, p2):
+        return ((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) ** 0.5
  
-    def cap(x: int) -> int:
-        replace = []
-        while x != comp[x]:
-            replace.append(x)
-            x = comp[x]
+    def check(h):
+        visited = [False] * len(dots)
+        dfs(0, h, visited)
+        return all(visited)
  
-        for p in replace:
-            comp[p] = x
-        return x
+    def dfs(v, h, visited):
+        visited[v] = True
+        for u in range(len(dots)):
+            if not visited[u] and dis(dots[v], dots[u]) <= h:
+                dfs(u, h, visited)
  
-    def unite(a: int, b: int) -> bool:
-        a, b = cap(a), cap(b)
-        if a == b:
-            return False
+    diameter = 2 * (2 * 10 ** 9)
+    l, r = 0, diameter
  
-        if size[a] < size[b]:
-            a, b = b, a
+    while r - l > 10 ** (-7):
+        mid = (l + r) / 2
+        if check(mid):
+            r = mid
+        else:
+            l = mid
  
-        comp[b] = a
-        size[a] += size[b]
-        return True
- 
-    def r2distance(d1: tuple[int, int], d2: tuple[int, int]) -> int:
-        dist = (d1[0] - d2[0]) ** 2 + (d1[1] - d2[1]) ** 2
-        return dist
- 
-    for i in range(n):
-        for j in range(i + 1, n):
-            r2 = r2distance(dots[i], dots[j])
-            distances.append((r2, i, j))
- 
-    distances.sort()
-    #print(distances)
-    n_components = n
-    for r2, i, j in distances:
-        if unite(i, j):
-            n_components -= 1
- 
-            if n_components == 1:
-                return math.sqrt(r2)
- 
-    return -1
+    return l
  
  
-if __name__ == '__main__':
+if __name__ == "__main__":
     n = int(input())
     d = [tuple(map(int, input().split())) for i in range(n)]
  
     result = solve(n, d)
-    print("%.12f" % result)
+    print("%.7f" % result)
