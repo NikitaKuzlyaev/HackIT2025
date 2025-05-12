84cb9faee9b777007a61c7c9bdb6896445b5936f
NikitaKuzlyaev <87369806+NikitaKuzlyaev@users.noreply.github.com>
2025-05-12 19:19:21 +0700

    Update d.py

--- d.py
+++ d.py
@@ -1,1 +1,11 @@
-
+def solve(notes):
+    if len(notes) > 26:
+        return 1
+    return 1 if len(set(notes)) < len(notes) else len(notes)
+ 
+ 
+if __name__ == "__main__":
+    t = int(input())
+    for i in range(t):
+        res = solve(input())
+        print(res)
