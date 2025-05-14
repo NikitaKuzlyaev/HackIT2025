6b511262619f95ea432f7be66b6724507d3d3231
NikitaKuzlyaev <87369806+NikitaKuzlyaev@users.noreply.github.com>
2025-05-14 13:12:28 +0700

    Update a.py

--- a.py
+++ a.py
@@ -1,1 +1,38 @@
 
+import sys
+
+
+def solve(n, m, d, l):
+    if m < n:
+        print('NO')
+        return
+
+    l.sort(reverse=True)
+    res = 0
+    right = m - 1
+    for left in range(m):
+        if left > right:
+            break
+
+        if l[left] >= d:
+            res += 1
+        else:
+            while right > left:
+                if l[right] + l[left] - 1 >= d:
+                    res += 1
+                    right -= 1
+                    break
+                else:
+                    right -= 1
+
+    if res >= n:
+        print('YES')
+    else:
+        print('NO')
+    return
+
+
+if __name__ == '__main__':
+    n, m, d = map(int, sys.stdin.readline().split())
+    l = list(map(int, sys.stdin.readline().split()))
+    solve(n, m, d, l)
